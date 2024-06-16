9d015b128ea55fb994c2480e7bda058ae04b5f3e
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-06-16 22:21:16 +0800

    Update server.js

--- project/server.js
+++ project/server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const axios = require('axios');
-const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname)));
 
 app.get('/api/get-video', async (req, res) => {
     try {
